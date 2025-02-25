a6f5cc58a6cc118734d56048585000339442b5ef
Mathias Grimm <mathiasgrimm@gmail.com>
2025-02-25 16:13:27 -0300

    wip

--- repo1/routes/web.php
+++ repo1/routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    logger('repo 1');
+    return 'ok: repo 1';
 });
--- repo2/routes/web.php
+++ repo2/routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    logger('repo 2');
+    return 'ok: repo 2';
 });
